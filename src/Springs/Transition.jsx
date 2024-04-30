0c7112b9cd5e3ae6f9ccf3ed8247eb7f1169a0f1
Apurba Sarkar <sarkar.apurba@hotmail.com>
2024-04-30 15:33:22 +0530

    others added

--- src/Springs/Transition.jsx
+++ src/Springs/Transition.jsx
@@ -1,6 +1,11 @@
-
-export default function Transition() {
-  return (
-    <div>Transition</div>
-  )
+import { useTransition, animated } from "@react-spring/web";
+export default function Transition({ data = [1, 2, 3] }) {
+  const [transitions, api] = useTransition(data, () => ({
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 1 },
+  }));
+  return transitions((style, item) => (
+    <animated.div style={style}>{item}</animated.div>
+  ));
 }
